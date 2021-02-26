2cb76d841200b9ad2a917487414ccaae96a305bc
carloshc <jchcchido@gmail.com>
2021-02-26 17:44:12 -0600

    Viernes 26 de Febrero

---
+++ src/main/java/mx/unam/aragon/fes/UsarPersona.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.aragon.fes;
+
+/**
+ *
+ * @author Charli
+ */
+public class UsarPersona {
+    public static void main(String[] args) {
+        
+        System.out.println("Programa usar persona");
+        
+        int edad = 0;
+        
+        Persona per1=new Persona(); // llamada al constructor
+        Persona per2=new Persona();
+        
+        System.out.println( per1.getNombre() );
+        
+        per2.setNombre("Carlos");
+        System.out.println( per2.getNombre() );
+        
+        System.out.println( per2.toString() );
+        per2.setEdad( 26 );
+        System.out.println( per2.toString() );
+        
+    }
+    
+}
